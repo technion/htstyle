537fd60e4c5b622d94ce92eef2f72f1a19c010b2
Technion <technion@lolware.net>
2015-07-08 04:17:06 +0000

    Implement a testable structure.

---
+++ lib/htstyle.rb
@@ -0,0 +1,14 @@
+module HTStyle
+  module_function
+  def indenter(handle=STDIN)
+    accum = 0
+    handle.each_line do |line|
+      line.lstrip!
+      accum += 4 if line.chomp[-1] == '{'
+      accum -= 4 if line.chomp[-1] == '}'
+      white = 80 - line.length - accum
+      white = 0 if white < 0
+      puts " " * white + line
+    end
+  end
+end
